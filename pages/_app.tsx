1f14240ac314206fb4d181b77f4fb60f18ae9040
huangliangshusheng <huangliangshusheng@gmail.com>
2021-05-17 16:14:29 +0800

    fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,11 @@ import "../styles/index.css";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider attribute="class">
+        <ThemeProvider
+            attribute="class"
+            storageKey="nightwind-mode"
+            defaultTheme="system" // default "light"
+        >
             <Component {...pageProps} />
         </ThemeProvider>
     );
