d0bcfb2abeacadb85b1223a7997005fc8bf6cc63
huangliangshusheng <huangliangshusheng@gmail.com>
2022-05-16 04:05:07 +0000

    fix bug

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -3,13 +3,13 @@ import { sampleUserData } from '../../../utils/sample-data'
 
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (!Array.isArray(sampleUserData)) {
-      throw new Error('Cannot find user data')
-    }
+      if (!Array.isArray(sampleUserData)) {
+          throw new Error("Cannot find user data");
+      }
 
-    res.status(200).json(sampleUserData)
-  } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message })
+      res.status(200).json(sampleUserData);
+  } catch (err: any) {
+      res.status(500).json({ statusCode: 500, message: err.message });
   }
 }
 
